9c2b5a2a61252451dd884da31e5e5f7f2c8ee0ad
Frank van Mourik <frank.vanmourik@poldervalley.com>
2021-12-11 22:19:14 +0100

    Day 11: part 2

--- day11/answer.py
+++ day11/answer.py
@@ -97,12 +97,99 @@ def result1(fileName):
 def result2(fileName):
     data = []
     for x in open(fileName):
-        data.append(x[:-1])
-    print(data)
+        data.append([int(y) for y in x[:-1]])
+    pprint(data)
+    new_data = [[(0, False)] * 10 for x in range(10)]
+    # pre processing
+    for j in range(len(data)):
+        for h in range(len(data[0])):
+            new_data[j][h] = (data[j][h], False)
+    data = new_data
+    for i in range(1000000):
+        # part 1
+        for j in range(len(data)):
+            for h in range(len(data[0])):
+                data[j][h] = (data[j][h][0] + 1, False)
+
+        # part 2
+        flashes_this_round = 0
+        octopusDidFlash = True
+        while (octopusDidFlash):
+            octopusDidFlash = False
+            for j in range(len(data)):
+                for h in range(len(data[0])):
+                    if data[j][h][0] > 9 and not data[j][h][1]:
+                        flashes_this_round += 1
+                        data[j][h] = (data[j][h][0], True)
+                        octopusDidFlash = True
+                        # Update neighbour cells
+                        if j == 0:
+                            if h == 0:
+                                data[j][h + 1] = (data[j][h + 1][0] + 1, data[j][h + 1][1])
+                                data[j + 1][h] = (data[j + 1][h][0] + 1, data[j + 1][h][1])
+                                data[j + 1][h + 1] = (data[j + 1][h + 1][0] + 1, data[j + 1][h + 1][1])
+                            elif h == len(data) - 1:
+                                data[j][h - 1] = (data[j][h - 1][0] + 1, data[j][h - 1][1])
+                                data[j + 1][h] = (data[j + 1][h][0] + 1, data[j + 1][h][1])
+                                data[j + 1][h - 1] = (data[j + 1][h - 1][0] + 1, data[j + 1][h - 1][1])
+                            else:
+                                data[j][h + 1] = (data[j][h + 1][0] + 1, data[j][h + 1][1])
+                                data[j + 1][h] = (data[j + 1][h][0] + 1, data[j + 1][h][1])
+                                data[j + 1][h + 1] = (data[j + 1][h + 1][0] + 1, data[j + 1][h + 1][1])
+                                data[j][h - 1] = (data[j][h - 1][0] + 1, data[j][h - 1][1])
+                                data[j + 1][h - 1] = (data[j + 1][h - 1][0] + 1, data[j + 1][h - 1][1])
+                        elif j == len(data) - 1:
+                            if h == 0:
+                                data[j][h + 1] = (data[j][h + 1][0] + 1, data[j][h + 1][1])
+                                data[j - 1][h] = (data[j - 1][h][0] + 1, data[j - 1][h][1])
+                                data[j - 1][h + 1] = (data[j - 1][h + 1][0] + 1, data[j - 1][h + 1][1])
+                            elif h == len(data) - 1:
+                                data[j][h - 1] = (data[j][h - 1][0] + 1, data[j][h - 1][1])
+                                data[j - 1][h] = (data[j - 1][h][0] + 1, data[j - 1][h][1])
+                                data[j - 1][h - 1] = (data[j - 1][h - 1][0] + 1, data[j - 1][h - 1][1])
+                            else:
+                                data[j][h + 1] = (data[j][h + 1][0] + 1, data[j][h + 1][1])
+                                data[j - 1][h] = (data[j - 1][h][0] + 1, data[j - 1][h][1])
+                                data[j - 1][h + 1] = (data[j - 1][h + 1][0] + 1, data[j - 1][h + 1][1])
+                                data[j][h - 1] = (data[j][h - 1][0] + 1, data[j][h - 1][1])
+                                data[j - 1][h - 1] = (data[j - 1][h - 1][0] + 1, data[j - 1][h - 1][1])
+                        else:
+                            if h == 0:
+                                data[j - 1][h] = (data[j - 1][h][0] + 1, data[j - 1][h][1])
+                                data[j + 1][h] = (data[j + 1][h][0] + 1, data[j + 1][h][1])
+                                data[j][h + 1] = (data[j][h + 1][0] + 1, data[j][h + 1][1])
+                                data[j + 1][h + 1] = (data[j + 1][h + 1][0] + 1, data[j + 1][h + 1][1])
+                                data[j - 1][h + 1] = (data[j - 1][h + 1][0] + 1, data[j - 1][h + 1][1])
+                            elif h == len(data) - 1:
+                                data[j - 1][h] = (data[j - 1][h][0] + 1, data[j - 1][h][1])
+                                data[j + 1][h] = (data[j + 1][h][0] + 1, data[j + 1][h][1])
+                                data[j][h - 1] = (data[j][h - 1][0] + 1, data[j][h - 1][1])
+                                data[j + 1][h - 1] = (data[j + 1][h - 1][0] + 1, data[j + 1][h - 1][1])
+                                data[j - 1][h - 1] = (data[j - 1][h - 1][0] + 1, data[j - 1][h - 1][1])
+                            else:
+                                data[j][h] = (data[j][h][0] + 1, data[j][h][1])
+                                data[j][h + 1] = (data[j][h + 1][0] + 1, data[j][h + 1][1])
+                                data[j][h - 1] = (data[j][h - 1][0] + 1, data[j][h - 1][1])
+                                data[j + 1][h] = (data[j + 1][h][0] + 1, data[j + 1][h][1])
+                                data[j + 1][h + 1] = (data[j + 1][h + 1][0] + 1, data[j + 1][h + 1][1])
+                                data[j + 1][h - 1] = (data[j + 1][h - 1][0] + 1, data[j + 1][h - 1][1])
+                                data[j - 1][h] = (data[j - 1][h][0] + 1, data[j - 1][h][1])
+                                data[j - 1][h + 1] = (data[j - 1][h + 1][0] + 1, data[j - 1][h + 1][1])
+                                data[j - 1][h - 1] = (data[j - 1][h - 1][0] + 1, data[j - 1][h - 1][1])
+
+        # Part 3
+        for j in range(len(data)):
+            for h in range(len(data[0])):
+                if data[j][h][1]:
+                    data[j][h] = (0, False)
+        if flashes_this_round == 100:
+            pprint(data)
+            return i+1
+    return 0
 
 
 def test():
-    print(result1("sample.txt"))
+    print(result2("sample.txt"))
 
 
 def result2_golf():
@@ -111,5 +198,5 @@ def result2_golf():
 
 if __name__ == "__main__":
     # print(test())
-    print(result1("input.txt"))
-    # print(result2("input.txt"))
+    # print(result1("input.txt"))
+    print(result2("input.txt"))
